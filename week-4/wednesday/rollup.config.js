1fa53866f84ddf53364c6826f5aa0684472b36ae
Isaac Sokari <isaacsokari@gmail.com>
2022-04-28 17:17:29 +0100

    feat: add banner and multiple format outputs

---
+++ week-4/wednesday/rollup.config.js
@@ -0,0 +1,19 @@
+import pkg from './package.json';
+
+const banner = `/*! ${pkg.name} v${pkg.version} | ${
+  pkg.description
+} | Copyright ${new Date().getFullYear()} | ${pkg.license} license */`;
+
+const formats = ['iife', 'cjs', 'es'];
+
+export default formats.map(function (format) {
+  return {
+    input: './src/time.js',
+    output: {
+      file: `./dist/time${format === 'iife' ? '' : `.${format}`}.js`,
+      format: 'iife',
+      name: 'Time',
+      banner: banner,
+    },
+  };
+});
