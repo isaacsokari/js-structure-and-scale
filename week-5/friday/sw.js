2eea6a48d854f8563d2440849b6e38687d212a21
Isaac Sokari <isaacsokari@gmail.com>
2022-05-08 12:39:34 +0100

    feat: add first service worker that shows an offline page.

---
+++ week-5/friday/sw.js
@@ -0,0 +1,37 @@
+// Listen for the install event
+self.addEventListener('install', function (event) {
+  // Activate immediately
+  self.skipWaiting();
+
+  // Cache the offline.html page
+  event.waitUntil(
+    caches.open('app').then(function (cache) {
+      cache.add(new Request('offline.html'));
+      return cache;
+    })
+  );
+});
+
+// Listen for request events
+self.addEventListener('fetch', function (event) {
+  // Get the request
+  let request = event.request;
+
+  // Bug fix
+  // https://stackoverflow.com/a/49719964
+  if (request.cache === 'only-if-cached' && request.mode !== 'same-origin')
+    return;
+
+  // HTML files
+  if (request.headers.get('Accept').includes('text/html')) {
+    event.respondWith(
+      fetch(request)
+        .then(function (response) {
+          return response;
+        })
+        .catch(function (error) {
+          return caches.match('offline.html');
+        })
+    );
+  }
+});
